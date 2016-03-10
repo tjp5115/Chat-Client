86a0bf80b71f0dc84e47875fbb802056193b25ff
blstrrifl <stl7199@rit.edu>
2016-03-10 15:54:18 -0500

    inital creation of database handler

---
+++ server/DatabaseHandler.java
@@ -0,0 +1,23 @@
+/* 
+* DataBaseHandler.java 
+* 
+* Version: 
+*  $Id$ 
+* 
+* Revisions: 
+*  $Log$ 
+*/
+
+//imports go here
+
+/* This class handles all requests from the clients, it also 
+makes the nessitary SQL calls to the database. 
+
+@authors: Samuel Launt, Tyler Paulsen, LAI CHUNG Lau
+@emails: stl7199@rit.edu, tjp5115@rit.edu, lxl3375@rit.edu 
+
+*/
+
+public class DatabaseHandler implements ClientListener{
+
+}//end class
